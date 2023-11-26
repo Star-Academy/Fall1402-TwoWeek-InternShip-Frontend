eb084842d5d0fc14dcdccf31639fd731c1ef382a
alirrah <alir.rah81@gmail.com>
2023-11-26 11:00:03 +0330

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {LandingComponent} from "./pages/landing/landing.component";
+import {NotFoundComponent} from "./pages/not-found/not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full' , component: LandingComponent},
+  { path: '**', component: NotFoundComponent },
+];
