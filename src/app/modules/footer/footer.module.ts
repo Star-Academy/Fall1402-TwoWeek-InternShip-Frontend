a9842ba885cc81403c35951c19c8e1a17b865013
alirrah <alir.rah81@gmail.com>
2023-11-30 10:37:22 +0330

    footer component

--- src/app/modules/footer/footer.module.ts
+++ src/app/modules/footer/footer.module.ts
@@ -3,14 +3,16 @@ import { CommonModule } from '@angular/common';
 import {FooterMenuComponent} from "../../components/footer-menu/footer-menu.component";
 import {FooterSocialComponent} from "../../components/footer-social/footer-social.component";
 import {FooterPhoneComponent} from "../../components/footer-phone/footer-phone.component";
+import {FooterComponent} from "../../components/footer/footer.component";
 
 
 @NgModule({
-  declarations: [FooterPhoneComponent, FooterMenuComponent, FooterSocialComponent],
+  declarations: [FooterPhoneComponent, FooterMenuComponent, FooterSocialComponent, FooterComponent,],
   exports: [
     FooterPhoneComponent,
     FooterMenuComponent,
     FooterSocialComponent,
+    FooterComponent,
   ],
   imports: [
     CommonModule
