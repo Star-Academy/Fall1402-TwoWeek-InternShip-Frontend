5d275557008cce7e531883fb62594cb75239389f
Mehrshad <mehrshad.farahani.jafari@gmail.com>
2023-11-30 14:05:07 +0330

    Convert offer css to sass

--- src/app/pages/landing/component/offer-game/offer-game.component.ts
+++ src/app/pages/landing/component/offer-game/offer-game.component.ts
@@ -1,10 +1,9 @@
 import {Component, Input} from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-offer-game',
   templateUrl: './offer-game.component.html',
-  styleUrl: './offer-game.component.css'
+  styleUrl: './offer-game.component.scss'
 })
 export class OfferGameComponent {
   @Input() items!: {image: string, title: string, description: string, oldPrice: string, price: string}[];
