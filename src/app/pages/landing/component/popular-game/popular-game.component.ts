794bf24e73df2f8362a2803f923d875e060fdcb6
Mehrshad <mehrshad.farahani.jafari@gmail.com>
2023-11-30 14:15:19 +0330

    Convert popular game css to sass

--- src/app/pages/landing/component/popular-game/popular-game.component.ts
+++ src/app/pages/landing/component/popular-game/popular-game.component.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-popular-game',
   templateUrl: './popular-game.component.html',
-  styleUrl: './popular-game.component.css'
+  styleUrl: './popular-game.component.scss'
 })
 export class PopularGameComponent {
   @Input() items!: {image: string, title: string, description: string}[];
